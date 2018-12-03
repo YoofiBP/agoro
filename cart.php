3bea2002e810ac3a3a073886b86a38f5a55e8768
nana <nana.obeng-marnu@ashesi.edu.gh>
2018-12-03 14:09:28 +0000

    “{obenfgdgge}”

--- cart.php
+++ cart.php
@@ -303,7 +303,7 @@ session_start();
 	</header>
 
 	<!-- Title Page -->
-	<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url(images/heading-pages-01.jpg);">
+	<section class="bg-title-page p-t-40 p-b-50 flex-col-c-m" style="background-image: url(images/cart.png);">
 		<h2 class="l-text2 t-center">
 			Cart
 		</h2>
