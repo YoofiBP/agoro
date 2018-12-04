702e5e4261ec3c33d41161863c6a5db673f2d420
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2018-12-04 03:27:50 +0000

    Made featured a template

--- js/ajax_calls.js
+++ js/ajax_calls.js
@@ -12,3 +12,18 @@ function show_header(){
   xmlhttp.open("GET","includes/templates/header.php",true);
   xmlhttp.send();
 }
+
+function show_featured(){
+  var xmlhttp;
+    xmlhttp=new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange=function()
+    {
+    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+      {
+      document.getElementById("featured").innerHTML=xmlhttp.responseText;
+      }
+    }
+  xmlhttp.open("GET","includes/templates/featured_products.php",true);
+  xmlhttp.send();
+}
