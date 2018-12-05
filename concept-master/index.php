bdd875e480e9504472d0ae7ebf31af3cfb43e6a5
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2018-12-05 04:42:11 +0000

    Corrected Index Navigation

--- concept-master/index.php
+++ concept-master/index.php
@@ -107,7 +107,7 @@ if(isset($_SESSION['admin'])){
                 <div id="submenu-4" class="collapse submenu" style="">
                   <ul class="nav flex-column">
                     <li class="nav-item">
-                      <a class="nav-link" href="pages/form-elements.html">View Entries</a>
+                      <a class="nav-link" href="tables/blogs.php">View Entries</a>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link" href="CRUD/blog_form.php">Add Entry</a>
